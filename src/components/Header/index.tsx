a6fb0531db6650db0fc2dca06f72f3cd2b9ee006
Fernando de Morais <mnando@gmail.com>
2020-10-15 02:28:49 -0300

    Atualiza o logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import logo from '../../assets/no-image.png'
+import logo from '../../assets/logo.svg'
 
 import '../../styles/globalStyles.css'
 import './styles.scss'
