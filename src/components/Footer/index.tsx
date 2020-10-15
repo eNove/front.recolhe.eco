9169125720fdeb8ea76d3d92ce7867bc1ad39b66
Fernando de Morais <mnando@gmail.com>
2020-10-15 02:44:05 -0300

    Implementa o footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
 
 import '../../styles/globalStyles.css'
-import './styles.css'
+import './styles.scss'
 
 const Footer: React.FC = () => {
   return (
     <>
       <footer>
-        <strong>Rodapé</strong>
+        <ul>
+          <li>
+            Atribuições:<br />
+            ● Vetores criados por freepik - br.freepik.com <br />
+          </li>
+          <li>
+            Conheça a api:<br />
+            <a href="http://api.recolhe.eco/api/v1/api-docs/" target="_blank" rel="noopener noreferrer">http://api.recolhe.eco/api/v1/api-docs/</a>
+          </li>
+          <li>
+            <a href="https://github.com/eNove/api.recolhe.eco" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithubSquare} /></a>
+          </li>
+        </ul>
       </footer>
     </>
   )
