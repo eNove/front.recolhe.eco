d9b8118574456faa02bf4c5269ebcddc564c5dba
Rafael Marquini <rafa.mardegan@gmail.com>
2020-10-15 11:42:08 -0300

    Fix conflicts

--- src/pages/CadastroColetor/index.tsx
+++ src/pages/CadastroColetor/index.tsx
@@ -4,7 +4,7 @@ import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 
 import '../../styles/globalStyles.css'
-import './styles.css'
+import './styles.scss'
 
 const CadastroColetor: React.FC = () => {
   return (
